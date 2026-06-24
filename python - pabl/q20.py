5002449e1ee4e405e647fa7b6de2d46a242cd8f0
syntaxlord <pushkar.kumar.cs28@iilm.edu>
2026-06-24 10:42:05 +0530

    Chocolate Distribution Problem

---
+++ python - pabl/q20.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findMinDiff(self, arr, m):
+
+        n = len(arr)
+
+        if m == 0 or n == 0:
+            return 0
+
+        arr.sort()
+
+        ans = float('inf')
+
+        for i in range(n - m + 1):
+
+            diff = arr[i + m - 1] - arr[i]
+
+            ans = min(ans, diff)
+
+        return ans
