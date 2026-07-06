d8ab81a32f5a966da076a7078fd98f1a759ea625
pushkar kumar <pushkar.kumar.cs28@iilm.edu>
2026-07-06 08:35:03 +0530

    Implement function to find duplicate number

---
+++ python - pabl/q26.py
@@ -0,0 +1,19 @@
+def findDuplicate(nums):
+    slow = nums[0]
+    fast = nums[0]
+    
+    while True:
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+        if slow == fast:
+            break
+    
+    slow = nums[0]
+    while slow != fast:
+        slow = nums[slow]
+        fast = nums[fast]
+    
+    return slow
+nums = [1, 3, 4, 2, 2]
+print("Input:", nums)
+print("Duplicate number:", findDuplicate(nums))
